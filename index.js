fc3a6b431db2241f7ee55abeab6d3e4c4585f001
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-02-22 21:03:56 +0000

    added password options

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 const bodyEl = document.querySelector("body");
 const containerEl = document.getElementById("container");
@@ -11,7 +12,8 @@ const lightDarkModeBtnEl = document.getElementById("light-dark-mode-btn");
 const generatedPasswordBtn1 = document.getElementById("generated-password-btn-1");
 const generatedPasswordBtn2 = document.getElementById("generated-password-btn-2");
 
-const passwordLength = 15;
+let password1 = "";
+let password2 = "";
 
 function changeDarkLightMode() {
   if (lightDarkModeBtnEl.textContent === "🌙") {
@@ -37,16 +39,47 @@ function changeDarkLightMode() {
 }
 
 function generatePasswords() {
-  generatedPasswordBtn1.textContent = generateRandomPassword()
-  generatedPasswordBtn2.textContent = generateRandomPassword()
+
+  password1 = generateRandomPassword();
+  password2 = generateRandomPassword();
+
+  if (password1.length > 15) {
+    generatedPasswordBtn1.textContent = password1.slice(0, 15) + "..."
+  }
+  else {
+    generatedPasswordBtn1.textContent = password1;
+  }
+
+  if (password2.length > 15) {
+    generatedPasswordBtn2.textContent = password2.slice(0, 15) + "..."
+  }
+  else {
+    generatedPasswordBtn2.textContent = password2;
+  }
 }
 
 function generateRandomPassword() {
   let password = "";
 
+  let passwordLength = document.getElementById("password-length").value;
+  let includeNumbers = document.getElementById("numbers").checked;
+  let includeSymbols = document.getElementById("symbols").checked;
+
+  let array = [];
+
+  array.push(...letters);
+
+  if (includeNumbers) {
+    array.push(...numbers);
+  }
+
+  if (includeSymbols) {
+    array.push(...symbols);
+  }
+
   for(let i = 0; i < passwordLength; i++) {
-    let randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
+    let randomIndex = Math.floor(Math.random() * array.length);
+    password += array[randomIndex];
   }
 
   return password;
@@ -55,7 +88,9 @@ function generateRandomPassword() {
 function copyFirstButtonToClipboard() {
   let buttonEl = document.getElementById("generated-password-btn-1");
 
-  navigator.clipboard.writeText(buttonEl.textContent).then(() => {
+  console.log(password1);
+
+  navigator.clipboard.writeText(password1).then(() => {
     alert("Copied to clipboard");
   }); 
 }
@@ -63,7 +98,12 @@ function copyFirstButtonToClipboard() {
 function copySecondButtonToClipboard() {
   let buttonEl = document.getElementById("generated-password-btn-2");
 
-  navigator.clipboard.writeText(buttonEl.textContent).then(() => {
+  navigator.clipboard.writeText(password2).then(() => {
     alert("Copied to clipboard");
   }); 
+}
+
+function handlePasswordLengthChange(input) {
+  if (input.value < 0) input.value = 0;
+  if (input.value > 99) input.value = 99;
 }
